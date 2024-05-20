7a31a064be2de6e2696642e41f44fac90d59cd5b
abir_zishan <rahman2007026@stud.kuet.ac.bd>
2024-05-20 06:12:38 +0600

    band display styling added

--- resources/views/bands.blade.php
+++ resources/views/bands.blade.php
@@ -4,32 +4,35 @@
 <title>Band Information</title>
 <meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
 <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-<link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
-<script src="{{ asset('js/jquery.js') }}"></script>
-<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<link href="{{ asset('css/bands.css') }}" rel="stylesheet">
 </head>
 <body>
 
 <div class="container mt-5">
     @foreach ($bands as $band)
-    <div class="band-detail">
-        <h2>{{ $band->name }}</h2>
-        <p>Genre: {{ $band->genre }}</p>
-        <p>Country: {{ $band->country }}</p>
-        <p>Founding Year: {{ $band->founding_year }}</p>
-        <p>Description: {{ $band->description }}</p>
-        <h3>Albums:</h3>
-        <ul>
-            @forelse ($band->albums as $album)
-                <li>{{ $album->title }} - Released on: {{ $album->release_date }}</li>
-            @empty
-                <li>No albums found.</li>
-            @endforelse
-        </ul>
+    <div class="band-card">
+        <div class="band-header">
+            <h2>{{ $band->name }}</h2>
+            <p class="band-genre">{{ $band->genre }}</p>
+        </div>
+        <div class="band-info">
+            <p><strong>Country:</strong> {{ $band->country }}</p>
+            <p><strong>Founding Year:</strong> {{ $band->founding_year }}</p>
+            <p>{{ $band->description }}</p>
+        </div>
+        <div class="band-albums">
+            <h3>Albums:</h3>
+            <ul>
+                @forelse ($band->albums as $album)
+                    <li>{{ $album->title }} - Released on: {{ $album->release_date }}</li>
+                @empty
+                    <li>No albums found.</li>
+                @endforelse
+            </ul>
+        </div>
     </div>
     @endforeach
 </div>
-
 
 <footer class="text-center mt-4">
     <p>All right reserved by Rhythmic Pulse - The Art of Music</p>
